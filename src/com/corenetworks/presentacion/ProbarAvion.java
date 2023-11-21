524078b7d4f3a5d560f42800ebcffcd26fa846f4
TitaRuiz <ggp2002es@gmail.com>
2023-11-21 19:11:47 +0100

    despues del descanso

--- src/com/corenetworks/presentacion/ProbarAvion.java
+++ src/com/corenetworks/presentacion/ProbarAvion.java
@@ -20,33 +20,36 @@ public class ProbarAvion {
 
         Scanner teclado = new Scanner(System.in);
         Pasajero p1 = new Pasajero();
-        int resultado=0;
+        int resultado = 0;
         System.out.println("Escriba el nombre del pasajero ->");
         p1.setNombre(teclado.nextLine());
         System.out.println("Escriba el DNI del pasajero ->");
         p1.setDni(teclado.nextLine());
         System.out.println("Escoga la clase \n 1) Ejecutivo \n 2) Turista \n");
         int clase = teclado.nextInt();
-        if (clase==1){
+        if (clase == 1) {
             System.out.println("Escoja la ubicacion \n 1) Ventana \n 2) Pasillo\n");
             int ubicacion = teclado.nextInt();
-            if (ubicacion==1){
+            if (ubicacion == 1) {
                 resultado = a1.asignarAsientoEjecutivo(p1, Ubicacion.VENTANA);
-            }else {
-                resultado = a1.asignarAsientoEjecutivo(p1,Ubicacion.PASILLO);
+            } else {
+                resultado = a1.asignarAsientoEjecutivo(p1, Ubicacion.PASILLO);
             }
 
 
-        }else{
+        } else {
             System.out.println("Escoja la ubicacion \n 1) Ventana \n 2)Centro \n  3) Pasillo\n");
             int ubicacion = teclado.nextInt();
-            switch (ubicacion){
-                case 1: resultado = a1.asignarAsientoTurista(p1,Ubicacion.VENTANA);
-                        break;
-                case 2: resultado = a1.asignarAsientoTurista(p1,Ubicacion.CENTRO);
-                        break;
-                case 3: resultado = a1.asignarAsientoTurista(p1,Ubicacion.PASILLO);
-                        break;
+            switch (ubicacion) {
+                case 1:
+                    resultado = a1.asignarAsientoTurista(p1, Ubicacion.VENTANA);
+                    break;
+                case 2:
+                    resultado = a1.asignarAsientoTurista(p1, Ubicacion.CENTRO);
+                    break;
+                case 3:
+                    resultado = a1.asignarAsientoTurista(p1, Ubicacion.PASILLO);
+                    break;
 
             }
         }
